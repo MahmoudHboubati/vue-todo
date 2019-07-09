9ed780d0970355ea1e41ded5db9a042504c9a879
Mahmoud Hboubati <mhmoud.21830@gmail.com>
2019-07-10 00:36:58 +0400

    feat(*): first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Todos</h1>
+    <div>
+      <div v-bind:key="todo.id" v-for="todo in todos">
+        <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+
+export default {
+  name: "Todos",
+  components: { TodoItem },
+  props: ["todos"],
+  methods: {}
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>
+      This is the vue.js crach that I have practice.
+    </p>
+  </div>
+</template>
+<script>
+export default {
+  name: "About"
+};
+</script>
